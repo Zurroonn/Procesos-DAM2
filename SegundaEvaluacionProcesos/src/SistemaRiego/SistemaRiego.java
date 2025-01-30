d9281e014d0de54fb7e4c9e3f2f7f88fd3ccadca
Alejandro Zurrón Pérez <aazurron@gmail.com>
2025-01-30 18:35:46 +0100

    commit

--- SegundaEvaluacionProcesos/src/SistemaRiego/SistemaRiego.java
+++ SegundaEvaluacionProcesos/src/SistemaRiego/SistemaRiego.java
@@ -1,5 +1,18 @@
 package SistemaRiego;
 
-public class SistemaRiego {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SistemaRiego extends TimerTask {
+
+	@Override
+	public void run() {
+		System.out.println("Regando...");
+	}
+	
+	public static void main(String[] args) {
+		Timer temporizador = new Timer();
+		temporizador.schedule(new SistemaRiego(), 5000, 2000);
+	}
 
 }
